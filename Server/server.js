227bc8c5b29a1158004ac45fffc2d33e80aa4cf4
paul <116600015+paulstllb@users.noreply.github.com>
2023-06-11 23:19:29 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-// Server starten
+// Server startennn
 app.listen(port, () => {
   console.log(`Server läuft auf Port ${port}`);
 });
