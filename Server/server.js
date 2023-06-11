95b4200c8e679fee08f4eba801cc88c875c783e5
paul <116600015+paulstllb@users.noreply.github.com>
2023-06-11 23:36:38 +0200

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Statische Dateien aus dem "build" -Ordner servieren
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Alle Anfragen, die nicht mit statischen Dateien übereinstimmen, an index.html weiterleiten
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+// Server startennn
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
